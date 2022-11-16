7918089bc2010df51bb182a5f6e79e45808d39bf
marvinxu99 <marvinxu99@hotmail.com>
2022-11-15 18:36:26 -0800

    inlcude files

---
+++ test2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  char name[100];
+  int age;
+
+  printf("Enter your name: ");
+  scanf("%s",name);
+  printf("Enter your age: ");
+  scanf("%d",&age);
+  printf("Hello, %s,You are %d years old\n", name, age);
+
+  return 0;
+}
---
+++ test_args.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(int argc,char* argv[]){
+  printf("Total number of arguments = %d\n",argc);
+  printf("Argument No. 1 = %s\n",argv[0]);
+  printf("Argument No. 2 = %s\n",argv[1]);
+  printf("Argument No. 3 = %s\n",argv[2]);
+}
